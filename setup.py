becd3022704212e41ff158529f5b9d4f09781d67
priezovic <jorispries@gmail.com>
2020-10-16 11:56:53 +0200

    updated url in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description = "Determine (optimal) baselines for binary classification",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/pypa/sampleproject",
+    url = "https://github.com/joris-pries/BinaryBaselines",
     packages = setuptools.find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
