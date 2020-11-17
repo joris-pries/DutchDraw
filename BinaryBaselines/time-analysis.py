d47905f4545d140e352a5df5052dd6626543d8c0
Etienne van de Bijl <etiennevandebijl@hotmail.com>
2020-11-17 11:02:01 +0100

    Working on time-analysis

--- BinaryBaselines/time-analysis.py
+++ BinaryBaselines/time-analysis.py
@@ -1,12 +1,13 @@
-
 import datetime
 import numpy as np
 import shuffle_baseline as sb
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.optimize import curve_fit
 
+# %%
 results = []
-for size in range(1,30):
+for size in range(1,50):
     print(size)
     for i in range(20):
         y_true = np.random.randint(2, size = size*10)
@@ -17,38 +18,30 @@ for size in range(1,30):
 
 df = np.array(results)
 df = pd.DataFrame(df, columns = ["Size","Time"])
-df["Size"] = df["Size"] * 10
 df.to_csv("C:/Users/Etienne/Documents/Github/BinaryBaselines/BinaryBaselines/time_size_g2.csv", index= False)
 
-df_new = df.groupby("Size")["Time"].mean().reset_index()
-
-def func(x, a, b, c):
-    return a * np.exp(-b * x) + c
-
-
-
-
-
+# %%
 plt.figure(figsize=(20, 20))
 plt.plot(df["Size"],df["Time"], ".")   
 plt.show()
 
+#%%
+df_new = df.groupby("Size")["Time"].mean().reset_index()
+
+# %%
+def func(x, a, b, c):
+    return a * b ** x + c
+
+popt, pcov = curve_fit(func, df_new["Size"].values, df_new["Time"].values)
+
+plt.plot( df_new["Size"].values, func( df_new["Size"].values, *popt), 'r-',
+         label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
+
+errors = [a - b for a,b in zip(func( df_new["Size"].values, *popt),df_new["Time"])]
+plt.plot(errors)
 
 
-from scipy.stats import hypergeom
-import math
 
-M = 100
-P = 10
-N = 90
-result = [np.nan] * (M + 1)
-for i in range(0, M + 1): # (M + 1)
-    theta = i / M
-    rounded_m_theta = round(round(M * theta))
-    TP_rv = hypergeom(M=M, n=P, N=rounded_m_theta)
-    lb = int(max(0, rounded_m_theta - N))
-    ub = int(min((P + 1, rounded_m_theta + 1)))
-    
-    result[i] = sum([(math.sqrt(k * (N - rounded_m_theta + k) / (P * N))) * TP_rv.pmf(k) for k in range(lb,ub)])
 
-M * P 
+
+
